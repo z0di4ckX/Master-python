f7217f8ffb71a79131fcf78d8e3e058020c32e14
Wesly <ultimatecracker88@gmail.com>
2020-04-10 17:33:15 -0400

    creat new file calling input and output

---
+++ 04-entrada-salida/entredad_salida.py
@@ -0,0 +1,6 @@
+# Entreda
+nombre = input("Cual es tu nombre: ")
+edad = input("Cual es tu edad: ")
+
+# Salida
+print(f"Me alegro de conocerte, bienvenido {nombre}, veo que tiene {int(edad) + 2} anos")
