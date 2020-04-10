678f275f95799337e099c04e9c0b3806c63cc1cb
Wesly <ultimatecracker88@gmail.com>
2020-04-10 17:21:57 -0400

    create file 03-operadores

---
+++ 03-operadores/aritmeticos.py
@@ -0,0 +1,18 @@
+# operadores aritmeticos
+
+numero1 = 77
+numero2 = 44 # operador asignacion = 
+
+resta = numero1 - numero2
+suma = numero1 + numero2
+multiplicaicon = numero1 * numero2
+division = numero1 / numero2
+resto = numero1 % numero2
+
+print("************* CALCULADORA *****************")
+
+print(f"La resta es: {resta}")
+print(f"La suma es: {suma}")
+print(f"La multiplicacion es: {multiplicaicon}")
+print(f"La division es: {division}")
+print(f"El resto de la division es: {resto}")
