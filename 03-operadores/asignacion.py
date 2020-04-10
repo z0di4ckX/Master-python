5147c737fba9dee52e1378186d8f8eea716ed5ce
Wesly <ultimatecracker88@gmail.com>
2020-04-10 17:27:51 -0400

    new file calling asginacion

---
+++ 03-operadores/asignacion.py
@@ -0,0 +1,35 @@
+# Operadores de asignacion
+
+edad = 55
+
+print(edad)
+
+edad += 5
+edad = edad + 5
+edad -= 5
+
+print(edad)
+
+# Operadores incremento y decremento
+
+year = 2021
+
+# Incremento
+year =  year + 1
+year += 1
+# Decremento
+year = year - 1
+year -= 1
+
+# Pre incremento
+year = 1 + year
+
+# Pre decremetno
+year = 1 - year
+
+"""
+    year++
+    year--
+    --year
+    ++year
+"""
