38ec393cd8a558073bcd44cd2f62923d2cc72cdd
Wesly <ultimatecracker88@gmail.com>
2020-04-10 00:46:06 -0400

    added new file curiosidades

---
+++ 01-holamundo/comen.py
@@ -0,0 +1,14 @@
+# Esto me mostrara el texto Hello por la consola
+print("Hello")
+
+# Estas dos instrucciones muestran texto por la pantalla
+print("World")
+print("!!!")
+
+"""
+Esto no se mostrara:
+    - print("Esto no se va a mostrar por pantalla")
+    - print("Por que estara dentro de un comentario mutilinias")
+"""
+
+print("Adios")
