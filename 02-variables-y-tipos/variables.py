6f8c0b65aa92f154e121cf0cdde9d51c8264d1b2
Wesly <ultimatecracker88@gmail.com>
2020-04-09 23:33:36 -0400

    added comment in variable file

--- 02-variables-y-tipos/variables.py
+++ 02-variables-y-tipos/variables.py
@@ -4,6 +4,33 @@ que dentro guardara un dato, se pueden crear
 muchas variables y que cada una tenga un dato distinto.
 """
 
+# Crear variable y darle un valor
 texto = "Master en python"
+texto2 = "con juan lopez"
+numero = 45
+decimal = 4.2
 
+# mostrar valor de los variables
 print(texto)
+print(texto2)
+print(numero)
+print(decimal)
+
+print("----------------")
+
+# Sustituir el valor de algunos variables / reasignar valores
+numero = 88
+decimal = 6.2
+
+print(numero)
+print(decimal)
+
+print("---------------------")
+# Concatenacion
+nombre = "Victor"
+apellidos = "Robles"
+web = "victorroblesweb.com"
+
+print(nombre + " " + apellidos + " " + web)
+print(f"{nombre} {apellidos} - {web}")
+print("Hola me llamo {} {} y mi web es: {}".format(nombre, apellidos, web))
