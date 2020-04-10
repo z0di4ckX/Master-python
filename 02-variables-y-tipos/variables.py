87dbff195827aff2a7a670004261210c642e4dfd
Wesly <ultimatecracker88@gmail.com>
2020-04-09 21:56:54 -0400

    create 02-variable-y-tipos file and create variable file

---
+++ 02-variables-y-tipos/variables.py
@@ -0,0 +1,9 @@
+""" 
+Una variables es un contenedor de informacion
+que dentro guardara un dato, se pueden crear
+muchas variables y que cada una tenga un dato distinto.
+"""
+
+texto = "Master en python"
+
+print(texto)
