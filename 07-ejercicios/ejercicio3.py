42620c690fc2741e8cae985445f3a90159b362bf
Wesly <ultimatecracker88@gmail.com>
2020-04-27 22:42:57 -0400

    new file calling ejercicio3

--- 07-ejercicios/ejercicio3.py
+++ 07-ejercicios/ejercicio3.py
@@ -4,9 +4,20 @@ Ejercicio 3. Escribir un programa que muestre los cuadrados
 naturales. Resolver con "for" y "while".
 """
 
+# WHILE
+
 contador = 0
 
 while contador <= 60:
     cuadrado = contador*contador
     print(f"El cuadrado {contador} es {cuadrado}")
     contador += 1
+
+
+print("#######################################")
+
+# FOR
+
+for numero in range(61):
+    cuadrado = numero*numero
+    print(f"El cuadrado {numero} es {cuadrado}")
